00edbb406ece092624f8338164dd05f00cadc95e
daldumalay <darell@xperto.ph>
2022-09-02 22:18:59 +0800

    Lawrence Fix profile picture members

--- theme/template-parts/layout/members-content.php
+++ theme/template-parts/layout/members-content.php
@@ -66,11 +66,13 @@
         <div class="flex w-full min-w-min bg-white rounded-lg p-6">
             <div class="flex flex-row items-start space-x-4 w-full">
                 <div class="w-20">
-                    <?php if (array_key_exists('mepr_profile_picture', $profile)) { ?>
-                        <a href="<?php echo $profile_link; ?>" class="hover:text-xperto-orange" alt="Visit profile" title="Visit Profile">
-                            <img src="<?php echo $profile['mepr_profile_picture']; ?>" class="rounded-full min-w-[80px] hover:border hover:border-xperto-orange" />
-                        </a>
-                    <?php } ?>
+                    <?php if (!empty($profile['mepr_profile_picture'])) : ?>
+                            <a href="<?php echo $profile_link; ?>" class="hover:text-xperto-orange" alt="Visit profile" title="Visit Profile">
+                                <img src="<?php echo $profile['mepr_profile_picture']; ?>" class="rounded-full border border-xperto-neutral-light-1 w-14 h-14" />
+                            </a>
+                        <?php else : echo get_avatar($current_user->ID, 68, '', 'avatar', array('class' => 'rounded-full border border-xperto-neutral-light-1 w-14 h-14')); 
+                        endif;
+                        ?>
                 </div>
                 <div class="w-full flex flex-col items-start space-y-4">
                     <header class="w-full">
