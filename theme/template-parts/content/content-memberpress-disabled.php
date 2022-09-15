b220a04015fd166cd0bcad7efc9f7561c8c706b3
Ginwene Rueda <gin@xperto.ph>
2022-09-15 22:40:02 +0800

    Added fail safe if memberpress plugin is disabled.

---
+++ theme/template-parts/content/content-memberpress-disabled.php
@@ -0,0 +1,12 @@
+<div class="bg-white w-full flex flex-col items-center justify-center h-screen">
+    <div class="p-4 mb-4 text-lg border border-xperto-error-dark text-xperto-error-dark bg-xperto-error-light rounded-lg flex flex-row space-x-2 items-center" role="alert">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 p-0 m-0 inline-block">
+            <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd" />
+        </svg>
+        <span>
+            A critical plugin was not enabled in the system. Please login to enable the plugin or message admin support.
+        </span>
+    </div>
+</div>
+
+<?php exit(); ?>
--- theme/template-parts/layout/header-memberpressproduct-content.php
+++ theme/template-parts/layout/header-memberpressproduct-content.php
@@ -8,17 +8,21 @@
  * @package xperto-ams
  */
 
+if (!is_plugin_active('memberpress/memberpress.php')) {
+	get_template_part('template-parts/content/content', 'memberpress-disabled');
+}
+
 ?>
 
 <div id="main-wrapper" class="w-full min-h-screen">
 	<header id="masthead" class="one-col-header w-full bg-white p-5 shadow-bottom top-0 z-10">
 		<div class="flex justify-center items-center h-10">
 			<?php
-			if (has_custom_logo()):
+			if (has_custom_logo()) :
 				the_custom_logo();
-			else: ?>
-				<a  href="<?php echo get_home_url(); ?>" class="h-full object-contain">
-					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/xperto_logo_min.png" class="h-full"/>
+			else : ?>
+				<a href="<?php echo get_home_url(); ?>" class="h-full object-contain">
+					<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/xperto_logo_min.png" class="h-full" />
 				</a>
 			<?php endif; ?>
 		</div>
